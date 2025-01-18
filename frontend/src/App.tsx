3e20a41039f95d9390101a2b00ec8452eec49e3c
Tetsuya Kato <tetsuyakato2001@yahoo.co.jp>
2025-01-19 00:18:04 +0900

    add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <div>testaaaa</div>
+        <div>testaaaaa</div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
