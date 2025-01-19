baf573c0609e302915fed5927ecb476a8715c8d1
Tetsuya Kato <47530581+empelt@users.noreply.github.com>
2025-01-19 14:25:00 +0900

    App.tsx の更新

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <div>testcd</div>
+        <div>testcde</div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
