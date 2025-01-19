af7763e27377a6255c6204e378a473f50006c494
Tetsuya Kato <47530581+empelt@users.noreply.github.com>
2025-01-19 23:18:18 +0900

    echoプロジェクト作成とcloud runへのデプロイのyaml作成 (#9)
    * echoプロジェクト作成とcloud runへのデプロイのyaml作成
    
    * レビュー対応
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * test
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * Revert "test"
    
    This reverts commit cf196ee50c7f51ae5b7f3948ef53747b36153dcd.
    
    * add
    
    * add
    
    ---------
    
    Co-authored-by: Mikage32 <mikage32@outlook.jp>

---
+++ backend/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	
+	"os"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+  if port == "" {
+      port = "8080"
+  }
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":"+port))
+}
