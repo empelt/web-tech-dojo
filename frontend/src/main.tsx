d5d63109767cfcf2d10b29f9a96c8258c8b0e12b
otsurob <tulaon0618@gmail.com>
2025-01-26 02:05:25 +0900

    ログイン画面が一番最初に表示されるように調整

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import App from './App'
 import './index.css'
+import LoginPage from './pages/Login'
 
 const root = document.getElementById('root')!
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
