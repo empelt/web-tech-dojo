30b8dce26f2f2e39f09940a2de29cfe0c579a5f9
Tetsuya Kato <tetsuyakato2001@yahoo.co.jp>
2025-01-31 02:16:18 +0900

    a

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,7 @@ const App = () => {
         console.log(res)
       })
       .catch((error) => {
-        console.error('リクエストエラーあ:', error)
+        console.error('リクエストエラーあい:', error)
       })
   }
 
