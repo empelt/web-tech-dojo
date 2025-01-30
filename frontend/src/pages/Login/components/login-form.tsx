42cc1853b7d28c0fe10c2061468d3264d86dbdce
otsurob <tulaon0618@gmail.com>
2025-01-30 19:19:21 +0900

    loginに関数受け渡しのひな型作成

--- frontend/src/pages/Login/components/login-form.tsx
+++ frontend/src/pages/Login/components/login-form.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { GalleryVerticalEnd } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
@@ -5,10 +7,21 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { cn } from '@/lib/utils'
 
+type LoginFormProps = React.ComponentPropsWithoutRef<'div'> & {
+  loginFunction: (email: string, password: string) => void
+  loginWithGoogle: () => void
+  switchToSignup: () => void
+}
+
 export const LoginForm = ({
   className,
+  loginFunction,
+  loginWithGoogle,
+  switchToSignup,
   ...props
-}: React.ComponentPropsWithoutRef<'div'>) => {
+}: LoginFormProps) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <form>
@@ -25,7 +38,10 @@ export const LoginForm = ({
             <h1 className="text-xl font-bold">Welcome to Acme Inc.</h1>
             <div className="text-center text-sm">
               Don&apos;t have an account?{' '}
-              <a className="underline underline-offset-4" href="#">
+              <a
+                className="underline underline-offset-4"
+                href="#"
+                onClick={switchToSignup}>
                 Sign up
               </a>
             </div>
@@ -35,16 +51,25 @@ export const LoginForm = ({
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="m@example.com"
                 required
                 type="email"
               />
             </div>
             <div className="grid gap-2">
-              <Label htmlFor="email">Password</Label>
-              <Input id="email" required type="email" />
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                type="password"
+              />
             </div>
-            <Button className="w-full" type="submit">
+            <Button
+              className="w-full"
+              onClick={() => loginFunction(email, password)}
+              type="submit">
               Login
             </Button>
           </div>
@@ -53,17 +78,10 @@ export const LoginForm = ({
               Or
             </span>
           </div>
-          {/* <div className="grid gap-4 sm:grid-cols-2"> */}
-          {/* <Button variant="outline" className="w-full">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path
-                  d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
-                  fill="currentColor"
-                />
-              </svg>
-              Continue with Apple
-            </Button> */}
-          <Button className="w-full" variant="outline">
+          <Button
+            className="w-full"
+            onClick={loginWithGoogle}
+            variant="outline">
             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -72,7 +90,6 @@ export const LoginForm = ({
             </svg>
             Continue with Google
           </Button>
-          {/* </div> */}
         </div>
       </form>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary  ">
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,9 +1,26 @@
+import { useNavigate } from 'react-router'
+
 import { LoginForm } from '@/pages/Login/components/login-form'
 const LoginPage = () => {
+  const navigate = useNavigate()
+  const switchToSignup = () => {
+    console.log('switchToSignup')
+    navigate('/signup')
+  }
+  const loginWithGoogle = () => {
+    console.log('loginWithGoogle')
+  }
+  const loginFunction = (email: string, password: string) => {
+    console.log(email, password)
+  }
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <LoginForm
+          loginFunction={loginFunction}
+          loginWithGoogle={loginWithGoogle}
+          switchToSignup={switchToSignup}
+        />
       </div>
     </div>
   )
