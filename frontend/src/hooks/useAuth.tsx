8b666442b7a7c039cb1e82141ca43e31c8b1ccd8
otsurob <tulaon0618@gmail.com>
2025-02-01 03:03:31 +0900

    ログイン情報を管理するhooksの作成

---
+++ frontend/src/hooks/useAuth.tsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+
+import { User, onAuthStateChanged, signOut } from 'firebase/auth'
+
+import auth from '@/libs/firebase'
+
+type AuthContextType = {
+  loading: boolean
+  logout: () => Promise<void>
+  user: User | null
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser)
+      setLoading(false)
+    })
+    return () => unsubscribe()
+  }, [])
+
+  const logout = async () => {
+    await signOut(auth)
+    setUser(null)
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, loading, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
