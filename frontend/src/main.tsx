d03d0efb63c58c276c17c7700e05396809a181c6
otsurob <tulaon0618@gmail.com>
2025-02-01 03:22:57 +0900

    AuthProviderでラップ

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,9 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './App'
 import { Header } from './components/Header'
 import './index.css'
+import { AuthProvider } from './hooks/useAuth'
 import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
 
 const root = document.getElementById('root')
 
@@ -17,11 +19,14 @@ if (!root) {
 ReactDOM.createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route element={<LoginPage />} path="/login" />
-        <Route element={<App />} path="/" />
-      </Routes>
+      <AuthProvider>
+        <Header />
+        <Routes>
+          <Route element={<LoginPage />} path="/login" />
+          <Route element={<SignupPage />} path="/signup" />
+          <Route element={<App />} path="/" />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
