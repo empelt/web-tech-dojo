91bb618f02e6081f92c814b9575ff742609843f7
otsurob <tulaon0618@gmail.com>
2025-02-01 00:14:45 +0900

    googleログイン処理の追加

--- frontend/src/pages/Login/components/login-form.tsx
+++ frontend/src/pages/Login/components/login-form.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import { CustomParameters, UserCredential } from 'firebase/auth'
 import { GalleryVerticalEnd } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
@@ -9,17 +10,21 @@ import { cn } from '@/lib/utils'
 
 type LoginFormProps = React.ComponentPropsWithoutRef<'div'> & {
   loginFunction: (email: string, password: string) => void
-  loginWithGoogle: () => void
+  signInWithGoogle: (
+    scopes?: string[],
+    customOAuthParameters?: CustomParameters,
+  ) => Promise<UserCredential | undefined>
   switchToSignup: () => void
 }
 
 export const LoginForm = ({
   className,
   loginFunction,
-  loginWithGoogle,
+  signInWithGoogle,
   switchToSignup,
   ...props
 }: LoginFormProps) => {
+  // const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   return (
@@ -78,20 +83,20 @@ export const LoginForm = ({
               Or
             </span>
           </div>
-          <Button
-            className="w-full"
-            onClick={loginWithGoogle}
-            variant="outline">
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                fill="currentColor"
-              />
-            </svg>
-            Continue with Google
-          </Button>
         </div>
       </form>
+      <Button
+        className="w-full"
+        onClick={() => signInWithGoogle()}
+        variant="outline">
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+            fill="currentColor"
+          />
+        </svg>
+        Continue with Google
+      </Button>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary  ">
         By clicking continue, you agree to our <a href="#">Terms of Service</a>{' '}
         and <a href="#">Privacy Policy</a>.
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,24 +1,43 @@
+import { useEffect } from 'react'
+
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
 import { useNavigate } from 'react-router'
 
+import auth from '@/libs/firebase'
 import { LoginForm } from '@/pages/Login/components/login-form'
+
 const LoginPage = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth)
   const navigate = useNavigate()
+  useEffect(() => {
+    if (user) {
+      console.log(user)
+      navigate('/')
+    }
+  }, [user, navigate])
   const switchToSignup = () => {
     console.log('switchToSignup')
     navigate('/signup')
   }
-  const loginWithGoogle = () => {
-    console.log('loginWithGoogle')
-  }
+
   const loginFunction = (email: string, password: string) => {
     console.log(email, password)
   }
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm
           loginFunction={loginFunction}
-          loginWithGoogle={loginWithGoogle}
+          signInWithGoogle={() => signInWithGoogle()}
           switchToSignup={switchToSignup}
         />
       </div>
