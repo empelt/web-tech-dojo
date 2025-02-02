2ccb23a23fc47e45b9e0218028a2409aa0c4ba7f
Tetsuya Kato <tetsuyakato2001@yahoo.co.jp>
2025-02-03 01:16:30 +0900

    ignorecase false

---
+++ frontend/src/pages/login/index.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useNavigate } from 'react-router'
+
+import { auth } from '@/lib/firebase'
+import LoginForm from '@/pages/login/components/form'
+
+const LoginPage = () => {
+  const [user] = useAuthState(auth)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (user) {
+      navigate('/questions')
+    }
+  }, [user, navigate])
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
