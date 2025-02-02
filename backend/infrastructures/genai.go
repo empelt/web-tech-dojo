26132742bc8656a30f5cd4558a2552027ec3610c
Mikage32 <mikage32@outlook.jp>
2025-02-03 01:28:29 +0900

    fix func for gemini api

--- backend/infrastructures/genai.go
+++ backend/infrastructures/genai.go
@@ -2,9 +2,9 @@ package infrastructures
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"cloud.google.com/go/vertexai/genai"
 )
@@ -26,17 +26,20 @@ func NewGenai(ctx context.Context) (*Genai, error) {
 
 func (g *Genai) GenerateContentFromText(ctx context.Context, message string) (string, error) {
 	gemini := g.Client.GenerativeModel(modelName)
-	prompt := genai.Text(message)
 
+	prompt := genai.Text(message)
 	resp, err := gemini.GenerateContent(ctx, prompt)
 	if err != nil {
 		return "", fmt.Errorf("error generating content: %w", err)
 	}
-	// See the JSON response in
-	// https://pkg.go.dev/cloud.google.com/go/vertexai/genai#GenerateContentResponse.
-	rb, err := json.MarshalIndent(resp, "", "  ")
-	if err != nil {
-		return "", fmt.Errorf("json.MarshalIndent: %w", err)
+	if resp.PromptFeedback != nil {
+		return "", fmt.Errorf("generating content is blocked: %s", resp.PromptFeedback.BlockReasonMessage)
 	}
-	return string(rb), nil
+	builder := strings.Builder{}
+	for _, candidate := range resp.Candidates {
+		for _, part := range candidate.Content.Parts {
+			builder.WriteString(fmt.Sprintf("%s\n", part))
+		}
+	}
+	return builder.String(), nil
 }
