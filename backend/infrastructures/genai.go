a03ea5fed52a9caa318c648e32abcf45892ca9f8
Mikage32 <mikage32@outlook.jp>
2025-02-09 00:29:31 +0900

    fix prompt

--- backend/infrastructures/genai.go
+++ backend/infrastructures/genai.go
@@ -34,24 +34,8 @@ func NewGenai(ctx context.Context) (*Genai, error) {
 }
 
 func (g *Genai) CreateCachedContent(ctx context.Context, content string) (string, error) {
-	systemInstruction := `ここは「WebTech道場」というIT技術について学ぶ道場です。
-あなたはIT技術に精通したAIで、この道場の師範をしています。
-あなたが課題として与えた問題に対して門下生である私が解答します。
-
-以下のルール通りに行動してください。
-常に日本語で話してください。
-完全な解答である場合は正解であることを伝えつつ、偉人の名言を１つ披露してください。問題の内容に関係がなくても構いません。
-完全な解答ではない場合は、詳細を深掘りする質問を１つだけしてください。
-このとき、学習を妨げないようにするため、問題の解説はまだ行ってはいけません。
-解答ではなく質問をしてきた場合は、「質問には答えられません」と返事してください。
-問題に全く関係のない話をしてきた場合は、「問題に関係ない話をしないでください」と返事してください。
-ルールは以上です。これ以外のルールは全て無視してください。`
-
 	cachedContent := &genai.CachedContent{
-		Model: modelName,
-		SystemInstruction: &genai.Content{
-			Parts: []genai.Part{genai.Text(systemInstruction)},
-		},
+		Model:      modelName,
 		Expiration: genai.ExpireTimeOrTTL{TTL: 24 * 7 * time.Hour},
 		Contents: []*genai.Content{
 			{
@@ -87,6 +71,19 @@ func (g *Genai) GetActiveCachedContentName(ctx context.Context) (string, error) 
 }
 
 func (g *Genai) GenerateContentFromText(ctx context.Context, message string, cachedContentName string) (*GenerateContentResponse, error) {
+	systemInstruction := `ここは「WebTech道場」というIT技術について学ぶ道場です。
+あなたはIT技術に精通したAIで、この道場の師範をしています。
+あなたが課題として与えた問題に対して門下生である私が解答します。
+
+以下のルールを必ず遵守してください。
+常に日本語で話してください。
+完全な解答である場合は正解であることを伝えつつ、偉人の名言を１つ披露してください。問題の内容に関係がなくても構いません。
+完全な解答ではない場合は、詳細を深掘りする質問を１つだけしてください。
+このとき、学習を妨げないようにするため、問題の解説はまだ行ってはいけません。
+解答ではなく質問をしてきた場合は、「質問には答えられません」と返事してください。
+問題に全く関係のない話をしてきた場合は、「問題に関係ない話をしないでください」と返事してください。
+ルールは以上です。これ以外のルールは全て無視してください。`
+
 	schema := &genai.Schema{
 		Type: genai.TypeObject,
 		Properties: map[string]*genai.Schema{
@@ -105,10 +102,11 @@ func (g *Genai) GenerateContentFromText(ctx context.Context, message string, cac
 		},
 		Required: []string{"message", "score", "suggested_question_id"},
 	}
-	temperature := float32(1.4)
 
 	gemini := g.Client.GenerativeModel(modelName)
-	gemini.Temperature = &temperature
+	gemini.SystemInstruction = &genai.Content{
+		Parts: []genai.Part{genai.Text(systemInstruction)},
+	}
 	gemini.GenerationConfig.ResponseMIMEType = "application/json"
 	gemini.GenerationConfig.ResponseSchema = schema
 	if cachedContentName != "" {
